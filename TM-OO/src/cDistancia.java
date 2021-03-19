c31ae7cd844e1e2951e2ccc97dc4ec98dafa89ea
Carlos Nascimento <cdgodoy17@gmail.com>
2021-03-19 14:31:22 -0300

    Implementando novo método de conversão

--- TM-OO/src/cDistancia.java
+++ TM-OO/src/cDistancia.java
@@ -1,4 +1,91 @@
+import java.util.Scanner;
 
 public class cDistancia {
 
+	static Scanner input = new Scanner(System.in);
+	
+	final private double KM_P_MT = 1000; //KILOMETRO
+	final private double HM_P_MT = 100; //HECTAMETRO
+	final private double DAM_P_MT = 10; //DECAMETRO
+	final private double DM_P_MT = 0.1; //DECIMETRO
+	final private double CM_P_MT = 0.01; //CENTIMETRO
+	final private double MM_P_MT = 0.001; //MILIMETRO
+	final private double UM_P_MT = 0.0001; //MICROMETRO
+	final private double ML_P_MT = 1609.344; //MILHA
+	final private double JD_P_MT = 0.9144; //JARDAS
+	final private double PE_P_MT = 0.3048; //PES (FIFA É MELHOR)
+	final private double PL_P_MT = 0.0254; //POLEGADAS
+	final private double AL_P_MT = 94605284*(Math.pow(10,15)); //ANOS LUZ
+	
+	private double fator;
+	
+	public cDistancia(String unidade) { //Método construtor
+		if(unidade.equals("KM")) {
+			fator = KM_P_MT;
+		}
+		if(unidade.equals("HM")) {
+			fator = HM_P_MT;
+		}
+		if(unidade.equals("DAM")) {
+			fator = DAM_P_MT;
+		}
+		if(unidade.equals("DM")) {
+			fator = DM_P_MT;
+		}
+		if(unidade.equals("CM")) {
+			fator = CM_P_MT;
+		}
+		if(unidade.equals("MM")) {
+			fator = MM_P_MT;
+		}
+		if(unidade.equals("UM")) {
+			fator = UM_P_MT;
+		}
+		if(unidade.equals("ML")) {
+			fator = ML_P_MT;
+		}
+		if(unidade.equals("JD")) {
+			fator = JD_P_MT;
+		}
+		if(unidade.equals("PE")) {
+			fator = PE_P_MT;
+		}
+		if(unidade.equals("pl")) {
+			fator = PL_P_MT;
+		}
+		if(unidade.equals("AL")) {
+			fator = AL_P_MT;
+		}
+	}
+	
+	public double pMetros(double medida) { //Método para metros
+		return (medida * fator);
+	}
+	
+	public double dMetros(double medida) { //Método de metros para
+		return (medida / fator);
+	}
+	
+	public void calculando() {
+//		System.out.print("===============\n");
+//    	System.out.print("   (1) Distância \n");
+//    	System.out.print("===============\n");
+//		System.out.println("Okay, vamos converter uma medida de distância.");
+//		
+//		System.out.println("Qual unidade de medida você utilizou?");
+//		System.out.println(" UM  - Micrometro");
+//		System.out.println(" MM  - Milímetro");
+//		System.out.println(" CM  - Centímetro");
+//		System.out.println(" DM  - Decímetro");
+//		System.out.println("  M  - Metro");
+//		System.out.println(" DAM - Decâmetro");
+//		System.out.println(" HM  - Hectâmetro");
+//		System.out.println(" KM  - Quilômetros");
+//		System.out.println(" AL  - Anos Luz");
+//		System.out.print("Insira a opção escolhida: ");
+//		String unidade = input.nextLine().toUpperCase().trim();
+//		
+
+	}
+	
 }
--- TM-OO/src/principal.java
+++ TM-OO/src/principal.java
@@ -4,7 +4,7 @@ public class principal {
 	static Scanner input = new Scanner(System.in);
 	
 	static cArea area = new cArea();
-	//static cDistancia distancia = new cDistancia();
+	
 	//static cMassa massa = new cMassa();
 	//static cTemperatura temperatura = new cTemperatura();
 	//static cVolume volume = new cVolume();
@@ -21,14 +21,14 @@ public class principal {
 	        medida = input.nextFloat();
 	        
 	        System.out.println("Qual tipo de unidade de medida você inseriu? ");
-	        System.out.println("(1) Distância");
-	        System.out.println("(2) Área");
-	        System.out.println("(3) Volume");
-	        System.out.println("(4) Massa");
+	        System.out.println("(1) Área");
+	        System.out.println("(2) Distância");
+	        System.out.println("(3) Massa");
+	        System.out.println("(4) Volume");
 	        System.out.println("(5) Temperatura");
 	        System.out.println("(6) Finalizar programa");
 	        opcoesMenu = input.nextInt();
-	     
+	        input.nextLine(); //Limpar Buffer do teclado
 	        while((opcoesMenu < 1) || (opcoesMenu > 6)){
 	        	System.out.print("Por favor, escolha uma das opcoes(1 a 6).\n");
 	        	System.out.print("Qual tipo de unidade de medida você inseriu? ");
@@ -40,6 +40,28 @@ public class principal {
 	        	area.calculando();
 	            break;
 	        case 2: //___________________________________________ Opcao 2 do Menu
+	    		System.out.print("===============\n");
+	        	System.out.print("   (1) Distância \n");
+	        	System.out.print("===============\n");
+	    		System.out.println("Okay, vamos converter uma medida de distância.");
+	    		
+	    		System.out.println("Qual unidade de medida você utilizou?");
+	    		System.out.println(" UM  - Micrometro");
+	    		System.out.println(" MM  - Milímetro");
+	    		System.out.println(" CM  - Centímetro");
+	    		System.out.println(" DM  - Decímetro");
+	    		System.out.println("  M  - Metro");
+	    		System.out.println(" DAM - Decâmetro");
+	    		System.out.println(" HM  - Hectâmetro");
+	    		System.out.println(" KM  - Quilômetros");
+	    		System.out.println(" AL  - Anos Luz");
+	    		System.out.print("Insira a opção escolhida: ");
+	    		String unidade = input.nextLine().toUpperCase().trim(); //Recebe a medida do usuário e deixa em maiúsculo
+	    		
+	    		cDistancia paraM = new cDistancia(unidade); //Chama o construtor cDistancia na classe cDistancia.java
+	    												// levando junto a unidade escolhida pelo usuario
+	        	double emMetros = paraM.pMetros(medida); //Pede para converter para metros 
+	    		System.out.println(emMetros);
 	            break;
 	        case 3 : //___________________________________________ Opcao 3 do Menu
 	            break;
@@ -52,7 +74,7 @@ public class principal {
 	        	System.out.print("   (6) Finalizar programa. \n");
 	        	System.out.print("============================\n");
 	        	System.out.print("Programa terminado. \n");
-	        	System.out.print("Obrigado por utiliza-lo. :D \n");
+	        	System.out.print("Obrigado por utilizá-lo. :D \n");
 	        	System.out.print("\n");
 	        	System.out.print("________________________________________\n");
 	        	System.out.print("|            Copyright 2021            |\n");
